2e9b74d2ba20d5967ff156f8f906d5c2d026c575
jihannbl <36852201+jihannbl@users.noreply.github.com>
2019-02-22 16:51:49 +0700

    Update soal3.sh

--- soal3.sh
+++ soal3.sh
@@ -2,9 +2,21 @@
 n="password"
 i=1
 
-while test -e "$n$i.txt"; do
- (( ++i ))
+function random(){
+   local inipass=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`
+   echo $inipass
+}
+
+pass=`random`
+
+while test -f "$n$i.txt"; do
+  if [ "$pass" == "`cat $n$i.txt`" ]
+  then
+   i=1
+   pass=`random`
+  else (( ++i ))
+  fi
 done
 
 fname="$n$i.txt"
-head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 > "$fname"
+echo $pass > "$fname"

5c2dfcd070683a2865e341fd5d15de402e886fde
jihannbl <36852201+jihannbl@users.noreply.github.com>
2019-02-22 14:04:37 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 cat /var/log/syslog | awk 'BEGIN {IGNORECASE = 1} /cron/ && !/sudo/ {print}' | awk 'NF < 13'
-
-crontab
-2-30/6 * * * * /home/jihan/nomer5.sh >> /home/jihan/modul1/nomer5.log 2>&1
